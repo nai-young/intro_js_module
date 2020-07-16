cfaf7c9a7c7ba3292dd7fba47019ef0131c9f5f7
N Young <60322086+nai-young@users.noreply.github.com>
2020-07-16 15:21:35 +0200

    Tipos de datos

---
+++ 03_datos.js
@@ -0,0 +1,15 @@
+// Tipos primitivos
+
+console.log(typeof undefined)
+console.log(typeof 32)
+console.log(typeof 'Pepe')
+console.log(typeof true)
+
+// Tipos referenciados
+console.log(typeof {})
+console.log(typeof [])
+
+// Numeros especiales
+
+const x = 2e500
+console.log(x * -x)
---
+++ 04_casting.js
@@ -0,0 +1,7 @@
+/* eslint-disable prefer-const */
+// A number
+let x = 2
+let y = '3'
+let z = x / y
+console.log(z)
+console.log(typeof z)
