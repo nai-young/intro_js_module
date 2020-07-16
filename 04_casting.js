4d622c952430f0a1521d51720bd16214660562b3
N Young <60322086+nai-young@users.noreply.github.com>
2020-07-16 18:00:17 +0200

    Objetos y Casting

--- 04_casting.js
+++ 04_casting.js
@@ -5,3 +5,17 @@ let y = '3'
 let z = x / y
 console.log(z)
 console.log(typeof z)
+
+// Casting forzado
+
+console.log(Boolean(0))
+console.log(Boolean(null))
+console.log(Boolean(NaN))
+console.log(Boolean(undefined))
+console.log(Boolean(10))
+
+let a = 2
+let b = '3'
+console.log(a * b)
+console.log(a + Number(b))
+console.log(a + +b)
---
+++ 05_objetos.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+
+// constantes y valores primitivos
+const pi = 3.14
+console.log(pi)
+
+const obj = {}
+console.log(typeof obj)
+console.log(obj)
+obj.nombre = 'Pepe'
+console.log(obj)
+
+// notación JSON u objetos literales
+const user = { nombre: 'Marta', edad: 34, fechaDeNacimiento: '23/01/2000' }
+console.log(user)
+
+// funciones constructoras
+const user2 = Object()
+user2.nombre = 'Carlos'
+user2.edad = 24
+console.log(user2)
+
+const datos = [1, 2, 3, 4, 5, 6]
+console.log(typeof datos)
+console.log(datos)
+
+// eslint-disable-next-line no-array-constructor
+const users = Array('Paula', 'Diana', 'Sofía')
+users[1] = 'Lucía'
+console.log(users)
+
+user.fechaDeNacimiento = new Date('2000/01/23')
+console.log(user)
+console.log(parseInt(Math.random() * 10))
+
+const cadena = 'Hola'
+console.log(cadena.length) // hace casting string to String
+const uper = cadena.toUpperCase()
+console.log(uper)
